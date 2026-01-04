839044dda71b4c7e5774185eee3d4b94ca1bd617
unknown <hbunce1561@gmail.com>
2026-01-04 16:53:11 -0500

    AI runtime optimizations

--- AI.java
+++ AI.java
@@ -1,9 +1,5 @@
 import java.util.ArrayList;
-<<<<<<< HEAD
 import java.util.HashMap;
-=======
->>>>>>> af710da7c1ff37077c59ee0e9aafd123f1e57b00
-
 public class AI extends Yacht {
     private int rollResults[];
     private SCORE_CATEGORIES c;
